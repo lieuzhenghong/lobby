17686a40f6d749615c492be401ec266a45f06bad
Donald Lee <dlqs@gmx.com>
2022-07-24 18:45:30 +0800

    asd

--- Lobby.gd
+++ Lobby.gd
@@ -83,7 +83,7 @@ class Message:
 		time = OS.get_time()
 		contents = contents
 	func _to_string() -> String:
-		return "%s: %s" % sender % contents
+		return "%s: %s" % [sender, contents]
 		# return ("%s [%s:%s:%s]: %s" % sender % time.hour % time.minute % time.second % contents)
 		
 puppet func _update_chat_log(chat_log):
